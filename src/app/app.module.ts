23cdc1a309df7841236d2afe06e9f4ec6a07d3d7
Rocky Bevins <rbevins@ad.pharmacord.com>
2017-02-09 13:15:42 -0500

    update to reflect base hero app, services and promise examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-
-import { AppComponent }  from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component'
 
 @NgModule({
   imports: [
@@ -10,9 +10,9 @@ import { AppComponent }  from './app.component';
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
-
 export class AppModule { }
