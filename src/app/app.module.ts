a2a98934beac569af1f7dede40afc4db81cb9fb2
Rocky Bevins <rbevins@ad.pharmacord.com>
2017-02-10 08:17:50 -0500

    adding base of http work, removing old mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+// in memory datastore mock
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +19,9 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
+    HttpModule
   ],
   declarations: [
     AppComponent,
